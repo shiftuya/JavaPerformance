b3999fbb0c2bae4e5e6ea2150ffa263aabcbc7e8
Simon Morozov <42973527+Morozzmorozov@users.noreply.github.com>
2021-05-18 13:50:31 +0700

    Update Main.java

--- Lab7/Main.java
+++ Lab7/Main.java
@@ -10,9 +10,9 @@ public class Main {
 
     File file = new File("main.bc");
     Source source = Source.newBuilder("llvm", file).build();
-    Value t = context.eval(source);
-    Value fn = t.getMember("getCpuInfo");
-    var cpuInfo = fn.execute().as(HashMap.class);
+    Value cpart = context.eval(source);
+    Value function = cpart.getMember("getCpuInfo");
+    var cpuInfo = function.execute().as(HashMap.class);
     System.out.println("Model name: " + cpuInfo.get("model name"));
     System.out.println("CPU cores: " + cpuInfo.get("cpu cores"));
     System.out.println("CPU MHz: " + cpuInfo.get("cpu MHz"));
